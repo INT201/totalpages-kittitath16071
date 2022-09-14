56be48f7ea00a272998b13d89b4f16161eef1410
kittitath16071 <kittitath4@gmail.com>
2022-09-14 23:13:04 +0700

    test02

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,16 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
-}
+  if (arrayItems === undefined || arrayItems === null) {
+    return undefined
+  } else if (rowsPerPage === undefined || rowsPerPage === null) {
+    return 1
+  } else if(rowsPerPage === 0){
+      return 1
+  } else {
+    return Math.ceil(arrayItems.length / rowsPerPage)
+  }
+  
+  
+
 module.exports = totalPages
